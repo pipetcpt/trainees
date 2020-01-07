34a59bbb453f05bda3be5052b65736117370d50a
Sungpil Han <pil.han@gmail.com>
2020-01-07 11:10:46 +0900

    update: files

--- 2020-homework/figure.r
+++ 2020-homework/figure.r
@@ -1,18 +1,24 @@
-library(pkr)
+# library ----
+
 library(NonCompart)
 library(ggplot2)
 library(readxl)
 library(dplyr)
 library(ncar)
-PK_data <- read_excel("Drug_X_PK.xlsx") %>% as.data.frame() %>% mutate(ID = as.factor(ID))
 
-ggplot(PK_data, aes(x = TIME, y = DV, group = ID, color = ID)) + 
-  geom_line() + 
-  labs(x = "Time", y = "concentration", title = "PK")
+# dataset ----
+
+PK_data <- read_excel("Drug_X_PK.xlsx") %>% as.data.frame() %>% mutate(ID = as.factor(ID))
 
 PK_GENE_1 <- PK_data %>% filter(GENE == 1)
 PK_GENE_2 <- PK_data %>% filter(GENE == 2)
 PK_GENE_3 <- PK_data %>% filter(GENE == 3)
+
+# figures ----
+
+ggplot(PK_data, aes(x = TIME, y = DV, group = ID, color = ID)) + 
+  geom_line() + 
+  labs(x = "Time", y = "concentration", title = "PK")
 
 ggplot(PK_GENE_1, aes(x = TIME, y = DV, group = ID, color = ID)) +
   geom_line() +
@@ -21,6 +27,7 @@ ggplot(PK_GENE_1, aes(x = TIME, y = DV, group = ID, color = ID)) +
 ggplot(PK_GENE_2, aes(x = TIME, y = DV, group = ID, color = ID)) +
   geom_line() +
   labs(x = "Time", y = "concentration", title = "PK_GENE2")
+
 ggplot(PK_GENE_3, aes(x = TIME, y = DV, group = ID, color = ID)) +
   geom_line() +
   labs(x = "Time", y = "concentration", title = "PK_GENE3")
